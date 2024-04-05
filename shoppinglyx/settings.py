ab5387c6a3cce5e8f9cbe568a2990561309ea4f9
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2024-04-05 12:17:37 +0530

    Update settings.py

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6$b#brfoa$!gg!ba(iwxu%k#r=3h^f1c4nd8ui&&#d4g-b#+#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
